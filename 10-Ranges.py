71cf180e960a18694f99ef992e15209f5556eafd
Sheetal Bisht <sheetalbisht41@gmail.com>
2023-08-31 20:35:38 +0530

    python

---
+++ 10-Ranges.py
@@ -0,0 +1,67 @@
+# for i in range(5):
+#     print(i)
+
+# OUTPUT :
+# 0
+# 1
+# 2
+# 3
+# 4
+
+
+
+# for i in range(3,10):
+#     print(i)
+
+#     OUTPUT:
+#     3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
+
+
+# for i in range(0,20,7):
+#     print(i)
+
+# OUTPUT
+# 0
+# 4
+# 8
+# 12
+# 16
+
+# burgers =['beef','chicken','veg','supreme','double']
+
+# for n in range(len(burgers)):
+#     print(n,burgers[n])
+
+#     OUTPUT
+
+#     0 beef
+# 1 chicken
+# 2 veg
+# 3 supreme
+# 4 double
+
+
+
+# //REVERSE LOOP
+
+# burgers =['beef','chicken','veg','supreme','double']
+
+# for n in range(len(burgers)-1,-1,-1):
+#     print(n,burgers[n])
+
+#     OUTPUT
+
+#     4 double
+# 3 supreme
+# 2 veg
+# 1 chicken
+# 0 beef
+
+
+
---
+++ 8-For_Loops.py
@@ -0,0 +1,26 @@
+sheetal =['situ','shreem','sita','sana']
+
+
+# //simple for loop
+# for i in sheetal:
+#     print(i) 
+
+
+
+#OUTPUT shreem  sita
+# for i in sheetal[1:3]:
+#     print(i)
+
+
+
+#OUTPUT 
+# # situ
+# shreem
+# sita-black belt
+
+# for i in sheetal:
+#     if i =='sita':
+#         print(f'{i}-black belt')
+#         break
+#     else:
+#      print(i)
