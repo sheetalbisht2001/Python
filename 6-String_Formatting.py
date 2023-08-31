dd5406c25a9c3388b5f66cb20d124cf036053a03
Sheetal Bisht <sheetalbisht41@gmail.com>
2023-08-31 19:30:21 +0530

    python basics

--- 6-String_Formatting.py
+++ 6-String_Formatting.py
@@ -1,1 +1,29 @@
-num1 = 
+num1 = 3.1425467389
+num2 =10.2903948
+
+# PREVIOUS
+# print('num 1 is',num1,'and num 2 is',num2)
+
+
+
+
+
+# FORMAT METHOD
+# print('num 1 is {0} and num 2 is {1}'.format(num1,num2))
+
+# print('num 1 is {0:.3} and num 2 is {1:.3}'.format(num1,num2))
+# num 1 is 3.14 and num 2 is 10.3  //OUTPUT
+
+# print('num 1 is {0:.3f} and num 2 is {1:.3f}'.format(num1,num2))
+# num 1 is 3.143 and num 2 is 10.290 //OUTPUT
+
+
+
+
+
+#USING F-STRINGS
+# print(f'num 1 is {num1} and num 2 is {num2}')
+# num 1 is 3.1425467389 and num 2 is 10.2903948
+
+# print(f'num 1 is {num1:.4f} and num 2 is {num2:.3f}')
+# num 1 is 3.1425 and num 2 is 10.290
