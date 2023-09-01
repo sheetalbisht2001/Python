d53c3504556a842e4d9f3c14e53b2232ddcff10d
Sheetal Bisht <sheetalbisht41@gmail.com>
2023-09-01 16:58:03 +0530

    python

--- 16-Init_Function.py
+++ 16-Init_Function.py
@@ -1,16 +1,35 @@
 class Planet:
 
-    def __init__(self):
-        self.name='Hoth'
-        self.radius=200000
-        self.gravity=5.5
-        self.system='Hoth System'
+    def __init__(self,name,radius,gravity,system):
+        self.name=  name
+        self.radius= radius
+        self.gravity= gravity
+        self.system= system
 
     def orbit(self):
       return f'{self.name} is orbiting in the {self.system}'
 
-hoth = Planet()    
+hoth = Planet('Hoth',2000,5.5,'Hoth System')    
 print(f'Name is:{hoth.name}')
 print(f'Radius is:{hoth.radius}')
 print(f'the gravity is:{hoth.gravity}')
 print(hoth.orbit())
+
+
+naboo = Planet('Naboo',3000,8,'Naboo System')    
+print(f'Name is:{hoth.name}')
+print(f'Radius is:{hoth.radius}')
+print(f'the gravity is:{hoth.gravity}')
+print(naboo.orbit())
+
+
+
+# OUTPUT
+# Name is:Hoth
+# Radius is:2000
+# the gravity is:5.5
+# Hoth is orbiting in the Hoth System
+# Name is:Hoth
+# Radius is:2000
+# the gravity is:5.5
+# Naboo is orbiting in the Naboo System
---
+++ 18-Modules_Packages.py
@@ -0,0 +1,8 @@
+from 17-Methods_Attributes import Planet
+
+
+
+naboo = Planet('Naboo',3000,8,'Naboo System') 
+print(f'Name:{naboo.name}')
+
+print(naboo.spin('a very high speed'))
